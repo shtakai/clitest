563fbb75d0bbdd6bec678faf5c07a816cc1dc2b1
Alyson Takai <shtakai@gmail.com>
2019-05-12 17:53:21 +0700

    Add binding form data

--- example.go
+++ example.go
@@ -10,11 +10,36 @@ type Person struct {
 	Name string `uri:"name" binding:"required"`
 }
 
+type StructA struct {
+	FieldA string `form:"field_a"`
+}
+
+type StructB struct {
+	NestedStruct StructA
+	FieldB string `form:"field_b"`
+}
+
+type StructC struct {
+	NestedStructPointer *StructA
+	FieldC string `form:"field_c"`
+}
+
+type StructD struct {
+	NestedAnonymousStruct struct {
+		FieldX string `form:"field_x"`
+	}
+	FieldD string `form:"field_d"`
+}
+
+
 func main() {
 	r := gin.Default()
 	r.GET("/users/:name/:id", userInfo())
 	r.GET("/ping", ping())
 	r.GET("/somejson", somejson())
+	r.GET("/getB", GetDataB())
+	r.GET("/getC", GetDataC())
+	r.GET("/getD", GetDataD())
 	r.Run()
 }
 
@@ -43,6 +68,39 @@ func ping() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+		})
+	}
+}
+
+func GetDataB() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var b StructB
+		c.Bind(&b)
+		c.JSON(200, gin.H{
+			"a": b.NestedStruct,
+			"b": b.FieldB,
+		})
+	}
+}
+
+func GetDataC() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var structC StructC
+		c.Bind(&structC)
+		c.JSON(200, gin.H{
+			"a": structC.NestedStructPointer,
+			"c": structC.FieldC,
+		})
+	}
+}
+
+func GetDataD() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var d StructD
+		c.Bind(&d)
+		c.JSON(200, gin.H{
+			"x": d.NestedAnonymousStruct,
+			"d": d.FieldD,
 		})
 	}
 }
