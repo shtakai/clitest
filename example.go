dee22a316356ce9ad45d2634cbcf69e12a8eb35a
Alyson Takai <shtakai@gmail.com>
2019-05-12 16:55:26 +0700

    Add binding uri

--- example.go
+++ example.go
@@ -1,24 +1,48 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+type Person struct {
+	ID string `uri:"id" binding:"required,uuid"`
+	Name string `uri:"name" binding:"required"`
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.GET("/somejson", func(c *gin.Context) {
+	r.GET("/users/:name/:id", userInfo())
+	r.GET("/ping", ping())
+	r.GET("/somejson", somejson())
+	r.Run()
+}
+
+func userInfo() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var person Person
+		if err := c.ShouldBindUri(&person); err != nil {
+			c.JSON(400, gin.H{"msg": err})
+			return
+		}
+		c.JSON(200, gin.H{"name": person.Name, "uuid": person.ID})
+	}
+}
+
+func somejson() gin.HandlerFunc {
+	return func(c *gin.Context) {
 		data := map[string]interface{}{
 			"lang": "GO言語",
 			"tag":  "<br>",
 		}
 		c.AsciiJSON(http.StatusOK, data)
-	})
-	r.Run()
+	}
+}
+
+func ping() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	}
 }
